26fbebd78f739148427ffece34c6794f01b239ca
Achudan <achuts4@gmail.com>
2022-12-05 12:50:47 -0500

    just like that

--- src/edu/asu/emit/qyan/alg/model/TravelRecommendation.java
+++ src/edu/asu/emit/qyan/alg/model/TravelRecommendation.java
@@ -9,13 +9,11 @@ import edu.asu.emit.qyan.alg.model.abstracts.BaseVertex;
 public class TravelRecommendation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Graph graph = new VariableGraph("data/mockData_v2_chk.csv");
 		YenTopKShortestPathsAlg yenAlg = new YenTopKShortestPathsAlg(graph);
 		List<Path> shortest_paths_list = yenAlg.get_shortest_paths(
 				graph.get_vertex(4), graph.get_vertex(5), 500, 6);
 		
-//		System.out.println(":"+shortest_paths_list);
 		for(Path path: shortest_paths_list) {
 			
 			ArrayList<String> routeList = new ArrayList<String>();
